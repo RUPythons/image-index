d648cb08df2460330734fc81db9a60d0c71f02f4
Joel W <joeljw@gmail.com>
2015-12-12 03:54:12 -0500

    Updated, working

--- image-search.py
+++ image-search.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 #
 #
-#
-''' This program will search a database of images and find the matching ones from a sourced image.'''
+# image-search.py
+''' This program will search a database of images and find the matching 
+ones from a sourced image.'''
 
 from PIL import Image
 import imagehash
 import argparse
 import shelve
+
+def ham_dist(str1, str2):
+    assert len(str1) == len(str2)
+    return sum(ch1 != ch2 for ch1, ch2 in zip(str1, str2))
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -25,14 +30,20 @@ db = shelve.open(args["shelve"])
 # load the query image, compute the difference image hash, and
 # and grab the images from the database that have the same hash
 # value
+filenames=[]
 query = Image.open(args["query"])
 h = str(imagehash.dhash(query))
-filenames = db[h]
-print "Found %d images" % (len(filenames))
+for ihash in db:
+    hd = ham_dist(h, ihash)
+    # print(hd)
+    if hd < 12:
+        filenames.append(str(db[ihash]).strip("[]'"))
+
+print ("Found %d images" % (len(filenames)))
 
 # loop over the images
 for filename in filenames:
-    image = Image.open(args["dataset"] + "/" + filename)
+    image = Image.open(args["dataset"] + "/" + str(filename))
     image.show()
 
 # close the shelve database
--- index.py
+++ index.py
@@ -13,6 +13,7 @@ import argparse
 import shelve
 import glob
 
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required = True,
@@ -22,13 +23,15 @@ ap.add_argument("-s", "--shelve", required = True,
 args = vars(ap.parse_args())
 
 # open the shelve database
-db = shelve.open(args["shelve"], writeback = True)
+db = shelve.open(args["shelve"], 'c', writeback = True)
+#db = {}
+#filename = open("patterns.pkl", "wb")
 # loop over the image dataset
 for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
     # load the image and compute the difference hash
     image = Image.open(imagePath)
     h = str(imagehash.dhash(image))
-
+    
     # extract the filename from the path and update the database
     # using the hash as the key and the filename append to the
     # list of values
@@ -36,5 +39,6 @@ for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
     db[h] = db.get(h, []) + [filename]
 
 # close the shelf database
+# pickle.dump(db, filename)
 db.close()
 
